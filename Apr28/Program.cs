05e0ff353b4ed83606ffaa1d50f8546842289b80
ccpaco <chrischampaco@gmail.com>
2021-04-30 14:55:52 -0700

    Add project files.

---
+++ Apr28/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Apr28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection Connector = new SqlConnection())
+            {
+                Connector.ConnectionString =
+                    ConfigurationManager.ConnectionStrings["c"].ConnectionString;
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = Connector;
+                    cmd.CommandText = "Select ProductName from products";
+
+                    using (SqlDataAdapter ad = new SqlDataAdapter())
+                    {
+                        DataSet d = new DataSet();
+                        ad.SelectCommand = cmd;
+                        ad.Fill(d);
+                        foreach(DataRow row in d.Tables[0].Rows)
+                        {
+                            Console.WriteLine(row.Field<string>("ProductName"));
+                        }
+                    }
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
